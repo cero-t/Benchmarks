4efb2191e5823f3f755831718d74c00eb8be1a87
Shin Tanimoto <cel@pos.to>
2013-11-07 11:17:39 +0900

    公開用

--- src/main/java/jp/co/acroquest/benchmark/ArrayListBenchmark.java
+++ src/main/java/jp/co/acroquest/benchmark/ArrayListBenchmark.java
@@ -2,26 +2,16 @@ package jp.co.acroquest.benchmark;
 
 import org.openjdk.jmh.Main;
 import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@State(Scope.Benchmark)
 public class ArrayListBenchmark {
     private static final int size = 100;
 
-    private List<Integer> sizedList;
-    private List<Integer> defaultList;
-
-    public static void main(String[] args) {
-        Main.main("-i 3 -wi 3 -f 1".split(" "));
-    }
-
     @GenerateMicroBenchmark
     public void withInitialSize() {
-        sizedList = new ArrayList<>(size);
+        List<Integer> sizedList = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
             sizedList.add(i);
         }
@@ -29,9 +19,13 @@ public class ArrayListBenchmark {
 
     @GenerateMicroBenchmark
     public void withoutInitialSize() {
-        defaultList = new ArrayList<>();
+        List<Integer> defaultList = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             defaultList.add(i);
         }
+    }
+
+    public static void main(String[] args) {
+        Main.main("-i 3 -wi 3 -f 1".split(" "));
     }
 }
